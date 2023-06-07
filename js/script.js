97366bf120950fb3c99f5883fa130e584b8fb76c
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-06-07 18:59:47 +0200

    fixed text position style

--- js/script.js
+++ js/script.js
@@ -106,7 +106,7 @@ createApp({
                 },
                 {
                     name: 'Claudia',
-                    avatar: './img/avatar_5.jpg',
+                    avatar: './img/avatar_io.jpg',
                     visible: true,
                     messages: [
                         {
